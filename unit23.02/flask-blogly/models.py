86faea7b3445895356da61e6981c9a97a6f1a8d6
SpicyOgurchik <107600965+SpicyOgurchik@users.noreply.github.com>
2023-02-20 21:18:23 -0500

    Add files via upload

---
+++ unit23.02/flask-blogly/models.py
@@ -0,0 +1,53 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
+class User(db.Model):
+    """User"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    @property
+    def full_name(self):
+        """Return full name"""
+
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return date"""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+def connect_db(app):
+    """Connect database"""
+
+    db.app = app
+    db.init_app(app)
